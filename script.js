4d6cb26fb7ff53c3d853e6490f06875673aa09ec
Liam Chivers <liamchivers2k9@hotmail.co.uk>
2022-07-30 01:55:24 +0100

    added toggle grid functionality + changed styling slightly

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const container = document.querySelector('.sketch-container')
 const changeGrid = document.querySelector('.grid-change-btn')
+const toggleGrid = document.querySelector('.toggleGrid')
 
+
+
+function getRandColor(){
+    const r = Math.floor(Math.random() * 255) 
+    const g = Math.floor(Math.random() * 255) 
+    const b = Math.floor(Math.random() * 255) 
+
+    return `rgb(${r}, ${g}, ${b})`
+}
 
 function createGrid(size = 64){
-
     container.style.gridTemplateRows = `repeat(${size}, auto)`;
     container.style.gridTemplateColumns = `repeat(${size}, auto)`;
 
     for(let i = 0; i < size*size; i++){
-
         const divs = document.createElement('div')
         container.append(divs)
 
@@ -17,7 +25,10 @@ function createGrid(size = 64){
         divs.addEventListener('mouseover', (e) => {
             e.target.style.backgroundColor = 'black'
         })
-        
+
+        toggleGrid.addEventListener('click', () => {
+            divs.style.border = 'none'
+        })
     }
 }
 createGrid()
@@ -25,5 +36,12 @@ createGrid()
 changeGrid.addEventListener('click', () => {
     container.textContent = ''
     const gridSquares = Math.floor(Math.abs(Number(prompt('enter a number of squares per side for the new grid.'))))
-    createGrid(gridSquares)
+
+    if(gridSquares > 100){
+        alert('Please insert only one number between 1-100')
+        createGrid()
+    } else {
+        createGrid(gridSquares)
+    }
+    
 })
