90a17097947a4054e8c904c85c30564c7ebbd994
Liam Chivers <85449720+liamchivers23@users.noreply.github.com>
2022-08-01 17:06:45 +0100

    added grid toggle functionality

--- script.js
+++ script.js
@@ -1,36 +1,39 @@
-const container = document.querySelector('.sketch-container')
-const changeGrid = document.querySelector('.grid-change-btn')
-const toggleGrid = document.querySelector('.toggleGrid')
+const container = document.querySelector(".sketch-container");
+const changeGrid = document.querySelector(".grid-change-btn");
+const toggleGrid = document.querySelector(".toggleGrid");
 
-function createGrid(size = 64){
-    container.style.gridTemplateRows = `repeat(${size}, auto)`;
-    container.style.gridTemplateColumns = `repeat(${size}, auto)`;
+function createGrid(size = 64) {
+  container.style.gridTemplateRows = `repeat(${size}, auto)`;
+  container.style.gridTemplateColumns = `repeat(${size}, auto)`;
 
-    for(let i = 0; i < size*size; i++){
-        const divs = document.createElement('div')
-        container.append(divs)
-      divs.classList.add('divs-border')
+  for (let i = 0; i < size * size; i++) {
+    const divs = document.createElement("div");
+    container.append(divs);
+    divs.classList.add("divs-border");
 
-        divs.addEventListener('mouseover', (e) => {
-            e.target.style.backgroundColor = 'black'
-        })
+    divs.addEventListener("mouseover", (e) => {
+      e.target.style.backgroundColor = "black";
+    });
 
-        toggleGrid.addEventListener('click', () => {
-            divs.classList.toggle('divs-border')
-        })
-    }
+    toggleGrid.addEventListener("click", () => {
+      divs.classList.toggle("divs-border");
+    });
+  }
 }
-createGrid()
+createGrid();
 
-changeGrid.addEventListener('click', () => {
-    container.textContent = ''
-    const gridSquares = Math.floor(Math.abs(Number(prompt('enter a number of squares per side for the new grid.'))))
+changeGrid.addEventListener("click", () => {
+  container.textContent = "";
+  const gridSquares = Math.floor(
+    Math.abs(
+      Number(prompt("enter a number of squares per side for the new grid."))
+    )
+  );
 
-    if(gridSquares > 100){
-        alert('Please insert only one number between 1-100')
-        createGrid()
-    } else {
-        createGrid(gridSquares)
-    }
-    
-})
+  if (gridSquares > 100) {
+    alert("Please insert only one number between 1-100");
+    createGrid();
+  } else {
+    createGrid(gridSquares);
+  }
+});
